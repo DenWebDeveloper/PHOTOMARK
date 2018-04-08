a336cddf26423a76934a3a7260be54bd0688e8bf
Denys <denv928@gmail.com>
2018-04-08 19:11:13 +0300

    Desktop версія готова

--- source/js/main.js
+++ source/js/main.js
@@ -1,23 +1,18 @@
-let swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    width: "1170",
-    slidesPerGroup: 3,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+new Swiper('.main-slider__container', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    initialSlide: 1,
+    spaceBetween: 20,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 });
 
-let reviewsSlider = new Swiper('.reviews-slider', {
+new Swiper('.reviews-slider', {
     slidesPerView: 2,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
