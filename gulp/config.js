4cbaff3e80392ca13914e163c87c88169e8f9b13
Denys <denv928@gmail.com>
2018-04-08 23:19:07 +0300

    Версія перед видаленням grid

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     root: './app',
 
-    autoprefixerConfig: ['last 3 version', '> 1%', 'ie 8', 'ie 9', 'Opera 12.1'],
+    autoprefixerConfig: ['last 5 version', '> 1%', 'ie 8', 'ie 9','ie 11', 'Opera 12.1'],
 
     fileCss: 'style.scss',
     fileJs: '*'
